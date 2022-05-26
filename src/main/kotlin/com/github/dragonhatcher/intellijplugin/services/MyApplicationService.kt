ca7da04d707e06150267d7f8f88c1527a69e81d8
GitHub Action <action@github.com>
2022-05-26 02:23:37 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/dragonhatcher/intellijplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.dragonhatcher.intellijplugin.services
+
+import com.github.dragonhatcher.intellijplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/test/kotlin/com/github/dragonhatcher/intellijplugin/MyPluginTest.kt
+++ src/test/kotlin/com/github/dragonhatcher/intellijplugin/MyPluginTest.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.dragonhatcher.intellijplugin
 
 import com.intellij.ide.highlighter.XmlFileType
 import com.intellij.psi.xml.XmlFile
